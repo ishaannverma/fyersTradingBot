0e200fab3705284e236b9e4ac501c6653cbf8100
ishaan <ishaanverma16@gmail.com>
2023-06-12 14:51:52 +0530

    add telegram to logging and add ping

--- modules/logging.py
+++ modules/logging.py
@@ -1,8 +1,14 @@
 import os
-
+from modules.telegram import sendTelegram
 
 class logger:
     path = ""
+    log_type = {
+        'INFO',
+        'DEBUG',
+        'WARNING',
+        'ERROR'
+    }
 
     def __init__(self):
         self.path = os.path.join(os.getcwd(), 'logs')
@@ -10,3 +16,10 @@ class logger:
             os.mkdir(self.path)
 
     # TODO: add option to send telegram of this too
+    def add_log(self, logType: str, message: str, sendPing: bool):
+        msg = f"{logType}: {message}"
+
+        #TODO: add to log file as well
+
+        if sendPing:
+            sendTelegram(msg)
--- modules/telegram.py
+++ modules/telegram.py
@@ -5,12 +5,16 @@ telegram_bot = telebot.TeleBot(telegram_credentials['API_KEY'])
 
 telegram_commands = {
     'help': 'Get all commands available with this bot',
+    'ping': 'Replies if running'
 
 }
 
 
 def sendTelegram(message):
     telegram_bot.send_message(1704843212, message)
+
+
+sendTelegram("Telegram Bot Running")
 
 
 @telegram_bot.message_handler(commands=['help'])
@@ -21,10 +25,12 @@ def help(message):
     telegram_bot.reply_to(message, text)
 
 
+@telegram_bot.message_handler(commands=['ping'])
+def ping(message):
+    telegram_bot.reply_to(message, "ping")
+
+
 # EASTER EGGS
 @telegram_bot.message_handler(commands=['sucha', 'swaru'])
 def test_telegram(message):
     telegram_bot.reply_to(message, "HEYYYY <3 <3")
-
-
-
